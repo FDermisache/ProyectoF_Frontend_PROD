76c7881f1f0ea8ca19d6fd1641ed8d9837a9a32c
Federico Dermisache <dermisache3@gmail.com>
2025-03-31 20:01:15 -0300

    arregloURL2

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -1,5 +1,5 @@
 const ENVIROMENT = {
-    URL_API: import.meta.env.VITE_URL_API || 'https://proyecto-f-backend-prod.vercel.app/',
+    URL_API: import.meta.env.VITE_URL_API || 'https://proyecto-f-backend-prod.vercel.app',
 }
 
 export default ENVIROMENT;
